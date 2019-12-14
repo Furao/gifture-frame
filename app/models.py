7f28d411138e650adad85963662c1b24b3f249fb
Furao <vanvossr@gmail.com>
2019-12-13 19:09:26 -0500

    Start implementing gif models and index page

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+from datetime import datetime
+
+class Gif(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    path = db.Column(db.String(120), index=True, unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    duration = db.Column(db.DateTime)
+    filesize = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<Gif {}>'.format(self.name)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    gifs = [
+        {'name': 'converse.gif', 'tags': ['animated', 'shoes']},
+        {'name': 'dance.gif', 'tags': ['live', 'cute']},
+    ]
+    return render_template('index.html', gifs=gifs)
